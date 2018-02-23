2d7ba5c9045ca0439059be76cd0d5ac420a7f2c0
ibabyblue <ibabyblue_z@outlook.com>
2018-02-23 17:53:21 +0800

    Initial Commit

---
+++ PCMHardEncodeToAAC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PCMHardEncodeToAAC
+//
+//  Created by ibabyblue on 2018/2/23.
+//  Copyright © 2018年 ibabyblue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PCMHardEncodeToAAC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PCMHardEncodeToAAC
+//
+//  Created by ibabyblue on 2018/2/23.
+//  Copyright © 2018年 ibabyblue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
